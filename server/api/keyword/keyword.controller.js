46b2f116cea5dd310cceb06b11ad8975eb48f09a
Jeremy Goodrich <jeremygoodrich@mac.com>
2014-10-27 18:13:33 -0700

    updates to keyword controller

--- server/api/keyword/keyword.controller.js
+++ server/api/keyword/keyword.controller.js
@@ -1,37 +1,37 @@
 /**
  * Using Rails-like standard naming convention for endpoints.
- * GET     /things              ->  index
- * POST    /things              ->  create
- * GET     /things/:id          ->  show
- * PUT     /things/:id          ->  update
- * DELETE  /things/:id          ->  destroy
+ * GET     /keywords              ->  index
+ * POST    /keywords              ->  create
+ * GET     /keywords/:id          ->  show
+ * PUT     /keywords/:id          ->  update
+ * DELETE  /keywords/:id          ->  destroy
  */
 
 'use strict';
 
 var _ = require('lodash');
-var Thing = require('./thing.model');
+var Keyword = require('./keyword.model');
 
 // Get list of things
 exports.index = function(req, res) {
-  Thing.find(function (err, things) {
+  Keyword.find(function (err, keywords) {
     if(err) { return handleError(res, err); }
-    return res.json(200, things);
+    return res.json(200, keywords);
   });
 };
 
 // Get a single thing
 exports.show = function(req, res) {
-  Thing.findById(req.params.id, function (err, thing) {
+  Keyword.findById(req.params.id, function (err, thing) {
     if(err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
+    if(!keyword) { return res.send(404); }
     return res.json(thing);
   });
 };
 
 // Creates a new thing in the DB.
 exports.create = function(req, res) {
-  Thing.create(req.body, function(err, thing) {
+  Keyword.create(req.body, function(err, thing) {
     if(err) { return handleError(res, err); }
     return res.json(201, thing);
   });
@@ -40,10 +40,10 @@ exports.create = function(req, res) {
 // Updates an existing thing in the DB.
 exports.update = function(req, res) {
   if(req.body._id) { delete req.body._id; }
-  Thing.findById(req.params.id, function (err, thing) {
+  Keyword.findById(req.params.id, function (err, keyword) {
     if (err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
-    var updated = _.merge(thing, req.body);
+    if(!keyword) { return res.send(404); }
+    var updated = _.merge(keyword, req.body);
     updated.save(function (err) {
       if (err) { return handleError(res, err); }
       return res.json(200, thing);
@@ -53,9 +53,9 @@ exports.update = function(req, res) {
 
 // Deletes a thing from the DB.
 exports.destroy = function(req, res) {
-  Thing.findById(req.params.id, function (err, thing) {
+  Keyword.findById(req.params.id, function (err, keyword) {
     if(err) { return handleError(res, err); }
-    if(!thing) { return res.send(404); }
+    if(!keyword) { return res.send(404); }
     thing.remove(function(err) {
       if(err) { return handleError(res, err); }
       return res.send(204);
