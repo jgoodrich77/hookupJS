67dc21304ec184dc668dd44b6c9bafe39f1de0db
levoir <nasir@levoirsolutions.com>
2014-11-01 13:02:15 +0530

    Keywords

--- server/api/listkeywords/index.js
+++ server/api/listkeywords/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./keyword.controller');
+var controller = require('./listkeyword.controller');
 
 var router = express.Router();
 
