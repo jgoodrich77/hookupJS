48986ad1ce7098bfd5fa3f57602e831bfd8328a8
levoir <nasir@levoirsolutions.com>
2014-11-03 09:46:25 +0530

    Keywords

--- server/routes.js
+++ server/routes.js
@@ -12,7 +12,7 @@ module.exports = function(app) {
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
   app.use('/api/keywords', require('./api/keyword'));
-  app.use('/api/listkeywords', require('./api/listkeywords'));
+ // app.use('/api/listkeywords', require('./api/listkeywords'));
   app.use('/auth', require('./auth'));
   
   // All undefined asset or api routes should return a 404
