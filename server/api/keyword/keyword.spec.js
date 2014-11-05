532475e545bc222461a502afc9e57c84b7cb9658
levoir <nasir@levoirsolutions.com>
2014-11-05 10:22:14 +0530

    Keywords

--- server/api/keyword/keyword.spec.js
+++ server/api/keyword/keyword.spec.js
@@ -4,11 +4,11 @@ var should = require('should');
 var app = require('../../app');
 var request = require('supertest');
 
-describe('GET /api/keyword', function() {
+describe('GET /api/keywords', function() {
 
   it('should respond with JSON array', function(done) {
     request(app)
-      .get('/api/keyword')
+      .get('/api/keywords')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function(err, res) {
--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,7 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
-  app.use('/api/keyword', require('./api/keyword'));
+  app.use('/api/keywords', require('./api/keyword'));
  // app.use('/api/listkeywords', require('./api/listkeywords'));
   app.use('/auth', require('./auth'));
   
