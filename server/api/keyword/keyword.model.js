ad84d81f40b95d12cae72d15cab3c12298a4b03e
levoir <nasir@levoirsolutions.com>
2014-11-05 10:11:15 +0530

    Keywords

--- server/api/keyword/keyword.model.js
+++ server/api/keyword/keyword.model.js
@@ -4,18 +4,22 @@ Schema = mongoose.Schema;
 
 var
 KeywordSchema = new Schema({
-  user_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'User'
-  },
-  keyword: {
-    type: String,
-    required: true
-  },
-  categories: [{
-    type: Schema.Types.ObjectId,
-    ref: 'KeywordCategory'
-  }]
+//  user_id: {
+//    type: Schema.Types.ObjectId,
+//    ref: 'User'
+//  },
+//  keyword: {
+//    type: String,
+//    required: true
+//  },
+//  categories: [{
+//    type: Schema.Types.ObjectId,
+//    ref: 'KeywordCategory'
+//  }]
+
+  keyword: String,
+  info: String,
+  active: Boolean
 });
 
 mongoose.model('Keyword', KeywordSchema);
