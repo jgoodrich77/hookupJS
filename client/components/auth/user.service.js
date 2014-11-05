54a494b64cab50d98c69739b03f4e718bd769646
levoir <nasir@levoirsolutions.com>
2014-11-05 14:07:16 +0530

    Keywords

--- client/components/auth/user.service.js
+++ client/components/auth/user.service.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('auditpagesApp')
-  .factory('User', function ($resource) {
+  .factory('Userd', function ($resource) {
     return $resource('/api/users/:id/:controller', {
       id: '@_id'
     },
