27347fc87e0508bfb5accd200a4b20b70a8e8c2c
levoir <nasir@levoirsolutions.com>
2014-11-07 15:52:56 +0530

    Keywords

--- server/api/keyword/keyword.spec.js
+++ server/api/keyword/keyword.spec.js
@@ -1,23 +1,23 @@
-//'use strict';
-//
-//var should = require('should');
-//var app = require('../../app');
-//var request = require('supertest');
-//var Keyword = require('./keyword.model');
-//
-//
-//describe('GET /api/keywords', function() {
-//
-//  it('', function(done) {
-//    request(app)
-//      .get('/api/keywords')
-//      .expect(200)
-//      .expect('Content-Type', /json/)
-//      .end(function(err, res) {
-//        if (err) return done(err);
-//        res.body.should.be.instanceof(Array);
-//        done();
-//      });
-//  });
-//  
-//});
+'use strict';
+
+var should = require('should');
+var app = require('../../app');
+var request = require('supertest');
+var Keyword = require('./keyword.model');
+
+
+describe('GET /api/keywords', function() {
+
+  it('', function(done) {
+    request(app)
+      .get('/api/keywords')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function(err, res) {
+        if (err) return done(err);
+        res.body.should.be.instanceof(Array);
+        done();
+      });
+  });
+  
+});
