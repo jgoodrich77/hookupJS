6923f4d888d595401a0bbf8d7555e7c731e5cfec
levoir <nasir@levoirsolutions.com>
2014-11-10 10:38:00 +0530

    Keywords

--- client/app/account/keyword/keyword.controller.js
+++ client/app/account/keyword/keyword.controller.js
@@ -1,48 +1,25 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 'use strict';
 angular
-.module('auditpagesApp')
-.controller('AccountKeywordsCtrl', function ($scope,$http, Auth, socket) {
-$scope.awesomeThings = [];
+        .module('auditpagesApp')
+        .controller('AccountKeywordsCtrl', function ($scope, $http, Auth, socket) {
+            $scope.keywords = [];
 
-    $http.get('/api/keywords').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-     socket.syncUpdates('keyword', $scope.awesomeThings);
-    });
-    $scope.errors = {};
+            $http.get('/api/keywords').success(function (keywords) {
+                $scope.keywords = keywords;
+                socket.syncUpdates('keyword', $scope.keywords);
+            });
+            $scope.errors = {};
 
-// $scope.addKeyword= function() {   
-//      $scope.submitted = true;
-//      Auth.saveKeyword({
-//    keyword:$scope.newKeyword
-//  })
-//        .then( function() {
-//          $location.path('/keyword');
-//        })
-//        .catch( function(err) {
-//          err = err.data;
-//          $scope.errors = {};
-//
-//          
-//        });
-//      
-//    };
+            $scope.addKeyword = function () {
+                if ($scope.newKeyword === '') {
+                    return;
+                }
+                $http.post('/api/keywords', {keyword: $scope.newKeyword});
+                $scope.newKeyword = '';
+            };
 
-$scope.addKeyword= function() {
-      if($scope.newKeyword=== '') {
-        return;
-      }
-      $http.post('/api/keywords', { keyword: $scope.newKeyword});
-      $scope.newKeyword = '';
-    };
+            $scope.deleteKeyword = function (keyword) {
+                $http.delete('/keyword/' + keyword._id);
+            };
 
-    $scope.deleteKeyword = function(keyword) {
-      $http.delete('/keyword/' + keyword._id);
-    };
-
-});
+        });
