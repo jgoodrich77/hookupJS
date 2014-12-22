765cab1f72da6d35a1964f5c2e8e2defdbdd2021
kryo <kryo2k@gmail.com>
2014-12-22 18:31:35 -0500

    Agenda test, seems like a good candidate for calendar opts

---
+++ test-code/test-agenda.js
@@ -0,0 +1,50 @@
+'use strict';
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var
+Q = require('q'),
+Agenda = require('agenda'),
+config = require('../server/config/environment');
+
+var
+when = 'in 10 seconds',
+agenda = new Agenda({
+  db: {
+    address: config.mongo.uri
+  }
+});
+
+agenda.define('post-facebook', function (job, done) {
+  console.log(job.attrs.data.time, 'hello world!');
+  done();
+});
+
+agenda.schedule(when, 'post-facebook', {
+  time: new Date()
+});
+
+agenda.start();
+
+agenda.on('start', function (job) {
+  console.log('Starting Job:', job.attrs.name)
+});
+
+agenda.on('complete', function (job) {
+  console.log('Completed Job:', job.attrs.name)
+});
+
+function graceful() {
+  console.log('Shutting down..');
+
+  agenda.stop(function() {
+    console.log('Exited gracefully.');
+    process.exit(0);
+  });
+}
+
+process.on('SIGTERM', graceful);
+process.on('SIGINT' , graceful);
+
+console.log('Wait..');
