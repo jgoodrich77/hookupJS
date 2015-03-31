f80f9975a8ecf67296e25297bfc28d08aac78e8d
kryo <kryo2k@gmail.com>
2015-03-31 06:46:45 -0400

    Prepped gruntfile for chart.js ability.

--- Gruntfile.js
+++ Gruntfile.js
@@ -246,7 +246,9 @@ module.exports = function (grunt) {
             '<%= yeoman.dist %>/public/{,*/}*.js',
             '<%= yeoman.dist %>/public/{,*/}*.css',
             '<%= yeoman.dist %>/public/assets/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-            '<%= yeoman.dist %>/public/assets/fonts/*'
+            '<%= yeoman.dist %>/public/assets/fonts/*',
+            '!<%= yeoman.dist %>/public/bower_components/Chart.js',
+            '!<%= yeoman.dist %>/public/bower_components/angular-chart.js'
           ]
         }
       }
@@ -266,7 +268,11 @@ module.exports = function (grunt) {
     usemin: {
       html: ['<%= yeoman.dist %>/public/{,*/}*.html'],
       css: ['<%= yeoman.dist %>/public/{,*/}*.css'],
-      js: ['<%= yeoman.dist %>/public/{,*/}*.js'],
+      js: [
+        '<%= yeoman.dist %>/public/{,*/}*.js',
+        '!<%= yeoman.dist %>/public/bower_components/Chart.js',
+        '!<%= yeoman.dist %>/public/bower_components/angular-chart.js'
+      ],
       options: {
         assetsDirs: [
           '<%= yeoman.dist %>/public',
