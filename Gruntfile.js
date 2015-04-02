140aaa08d605630061cbe09f6b5432b39816e4c3
kryo <kryo2k@gmail.com>
2015-04-02 19:12:04 -0400

    Included google webmaster tools as part of the build process.

--- Gruntfile.js
+++ Gruntfile.js
@@ -402,6 +402,7 @@ module.exports = function (grunt) {
             'bower_components/**/*',
             'assets/images/{,*/}*.{webp}',
             'assets/fonts/**/*',
+            'google*.html',
             'index.html'
           ]
         }, {
